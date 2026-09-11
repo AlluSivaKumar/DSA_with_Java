178b4c92be6ad2bda1433e8020b8c48e3f47c952
AlluSivaKumar <sivakumarallu4@gmail.com>
2026-09-11 20:39:14 +0530

    Sept 11 Code Updated

--- LeetCode/LeetCode3483.java
+++ LeetCode/LeetCode3483.java
@@ -1,23 +1,77 @@
 package LeetCode;
 
-import java.util.Arrays;
-
 public class LeetCode3483 {
     public static void main(String[] args) {
-        totalNumbers(new int[] {1,2,3,4,5});
+        System.out.println(totalNumbers(new int[] {1,2,3,4,5}));
     }
 
-    public static void totalNumbers(int[] digits) 
+    public static int totalNumbers(int[] digits) 
     {
         int[] freq = new int[10];
 
-        for(int i=0;i<10;i++)
+        for(int digit : digits)
         {
-            freq[i]++;
+            freq[digit]++;
         }
 
-        //CHOOSE FROM CONSTRAINS - NUMBER SHOULD BE THREE DIGIT NUMBER
+        int count = 0;
 
-        System.out.println(Arrays.toString(freq));
+        //CHOOSE FROM CONSTRAINS - NUMBER SHOULD BE THREE DIGIT NUMBER
+        for(int num=100;num<=999;num++)
+        {
+            if(num % 2 != 0)
+            {
+                continue;
+            }
+
+            int[] com = new int[10];
+            int temp = num;
+
+            int a = temp % 10;
+            temp = temp / 10;
+            com[a]++;
+
+            int b = temp % 10;
+            temp = temp / 10;
+            com[b]++;
+
+            int c = temp % 10;
+            temp = temp / 10;
+            com[c]++;
+
+            if(com[a] <= freq[a] && com[b] <= freq[b] && com[c] <= freq[c])
+            {
+                count++;
+            }
+        }
+
+        return count;
+
     }
 }
+
+
+/* 
+class Solution {
+    public int totalNumbers(int[] digits) {
+        HashSet<Integer> map = new HashSet<>();
+        int n = digits.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (i != j && i != k && j != k) {
+                        if (digits[i] != 0) {
+                            if (digits[k] % 2 == 0) {
+                                int number = digits[i] * 100 + digits[j] * 10 + digits[k];
+                                map.add(number);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return map.size();
+    }
+}
+ */
